749dca5dffcae445cf037d4a43b39310c8dfd356
Satyam Ray <satyamray12@gmail.com>
2019-07-14 01:17:40 +0530

    initial commit

---
+++ src/main/java/com/satyamcs12/calender/CalenderApplication.java
@@ -0,0 +1,14 @@
+package com.satyamcs12.calender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalenderApplication.class, args);
+	}
+
+
+}
